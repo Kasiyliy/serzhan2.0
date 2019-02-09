1a6b8fd57c1eb4755833ee4f85ac0bf0bd441040
air <asilkhan-al@mail.ru>
2019-02-10 04:13:05 +0600

    ENTIRE CRUD FOR CATEGORIES IS DONE

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -6,19 +6,24 @@
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">
-                        <h2>Добавить категорию</h2>
+                        <h2>Изменить категорию</h2>
                         <a  class="btn btn-primary btn-sm" href="{{route('category.index')}}">Назад</a>
                     </div>
                     <div class="card-body">
-                        <form action="{{route('category.store')}}" method="post">
+                        <form action="{{route('category.update' ,['id'=>$category->id])}}" method="post">
                             <div class="form-group">
                                 <label for="name">Имя</label>
-                                <input type="text" name="name" class="form-control" placeholder="Наименование" required>
+                                <input type="text" value="{{$category->name}}" name="name" class="form-control" placeholder="Наименование" required>
                             </div>
                             {{csrf_field()}}
                             <div class="form-group">
-                                <input type="submit" class="btn btn-success btn-block" value="Добавить">
+                                <input type="submit" class="btn btn-outline-primary btn-block" value="Изменить">
                             </div>
+                            @if($errors)
+                                @foreach($errors->all() as $error)
+                                    <p class="m-1 text-danger">{{$error}}</p>
+                                @endforeach
+                            @endif
                         </form>
                     </div>
                 </div>
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">
                         <h2>Категории</h2>
@@ -15,6 +15,7 @@
                                 <tr>
                                     <th>ID</th>
                                     <th>Наименование</th>
+                                    <th>Действия</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -22,6 +23,13 @@
                                     <tr>
                                         <td>{{$category->id}}</td>
                                         <td>{{$category->name}}</td>
+                                        <td class="d-flex">
+                                            <form method="post" action="{{route('category.delete', ['id' => $category->id ])}}">
+                                                {{csrf_field()}}
+                                                <input type="submit" value="Удалить" class="btn btn-danger btn-sm mr-1">
+                                            </form>
+                                            <a href="{{route('category.edit' ,['id'=>$category->id ])}}" class="btn-sm btn btn-primary">Изменить</a>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </tbody>
