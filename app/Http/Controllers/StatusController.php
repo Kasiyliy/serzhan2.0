a6db2b99576e31e0f87add78d83db53577352226
Rassh97 <khamitsasuke97@gmail.com>
2019-02-10 06:13:24 +0600

    Yera's first commit

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -42,4 +42,49 @@ class StatusController extends Controller
             return redirect()->back();
         }
     }
+
+
+
+    public function delete($id){
+        $status = Status::find($id);
+        if($status){
+            $status->delete();
+            Session::flash('success' , 'Статус успешно удален!');
+        }else{
+            Session::flash('error' , 'Статус не существует!');
+        }
+        return redirect()->back();
+    }
+
+    public function edit($id){
+        $status = Status::find($id);
+        if(!$status){
+            Session::flash('error' , 'Статус не существует!');
+            return redirect()->back();
+        }
+
+        return view('admin.statuses.edit', compact('status'));
+    }
+
+    public function update(Request $request, $id){
+        $status = Status::find($id);
+        if(!$status){
+            Session::flash('error' , 'Статус не существует!');
+            return redirect()->back();
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' =>'required'
+        ]);
+
+        if ($validator->fails()) {
+            Session::flash('error' , 'Ошибка!');
+            return redirect()->back()->withErrors($validator);
+        }else{
+            $status->name = $request->name;
+            $status->save();
+            Session::flash('success' , 'Статус успешно обновлен!');
+            return redirect()->back();
+        }
+    }
 }
