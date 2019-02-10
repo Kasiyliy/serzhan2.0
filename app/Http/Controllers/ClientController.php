2c9fc535631c0f98398037debd1dbb922306b49b
zheniserov <zheniserov@gmail.com>
2019-02-10 06:13:21 +0600

    delete,update

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -47,5 +47,48 @@ class ClientController extends Controller
     }
 
 
+    public function delete($id){
+        $client = Client::find($id);
+        if($client){
+            $client->delete();
+            Session::flash('success' , 'Клиент успешно удлен!');
+        }else{
+            Session::flash('error' , 'Такого клиента не существует!');
+        }
+        return redirect()->back();
+    }
+
+    public function edit($id){
+        $client = Client::find($id);
+        if(!$client){
+            Session::flash('error' , 'Такого клиента не существует!');
+            return redirect()->back();
+        }
+
+        return view('admin.clients.edit', compact('category'));
+    }
+
+    public function update(Request $request, $id){
+        $category = Category::find($id);
+        if(!$category){
+            Session::flash('error' , 'Такого клиента не существует!');
+            return redirect()->back();
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' =>'required'
+        ]);
+
+        if ($validator->fails()) {
+            Session::flash('error' , 'Ошибка!');
+            return redirect()->back()->withErrors($validator);
+        }else{
+            $category->name = $request->name;
+            $category->save();
+            Session::flash('success' , 'Категория успешно обновлена!');
+            return redirect()->back();
+        }
+    }
+
     
 }
